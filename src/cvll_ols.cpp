7deba9c016d0fa63afec32a5e4d1c7acde07d624
Shana Scogin <scogins@gmail.com>
2019-03-12 23:33:37 -0400

    Dying slowly

--- src/cvll_ols.cpp
+++ src/cvll_ols.cpp
@@ -8,39 +8,38 @@
  * @return void
  */
 
-#include <math.h> // not sure if needed
-#include "R.h" // not sure if needed
 #include <RcppArmadillo.h>
-#include <RcppEigen.h> // for arma::solve
+#include <math.h>
+#include "R.h"
+#include "R_ext/Memory.h"
+// #include <RcppEigen.h> // for arma::solve or fastLm or something? not sure if nec
 //[[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
-using namespace std; //not sure if needed
 using namespace arma;
 
 // [[Rcpp::export}}]]
+NumericVector cvll_ols(NumericMatrix x, NumericVector y) { //dmatrix for x?
 
-NumericVector cvll_ols(NumericMatrix *x, NumericVector *y, int *n_row) { //dmatrix for x?
-// need to add nrow to r file wrapper function
-
-  NumericVector cvll_ls ;
-  NumericVector yt ;
+  int n_row = y.size() ;
+//  NumericVector cvll_ls(n_row) ;
+  NumericVector yt(n_row - 1) ;
   NumericMatrix xt ;
-  NumericVector yv ; // this is actually a double but cpp gets angry bc y is vector
+  NumericVector yv(1) ; // this is actually a double but cpp gets angry bc y is vector
   NumericMatrix xv ; // should this be vector? it's vector of ivs
-  double ls ;
-  double sig ;
+//  List ls ;
+//  double sig ;
 
   // resource: https://teuder.github.io/rcpp4everyone_en/100_matrix.html
-  for (int i = 0; i <* n_row; i++) { //check <* rather than <
+  for (int i = 0; i < n_row; i++) { //check <* rather than <?
     yt = y[-i] ; // leaves out observation i for out of sample
     xt =  x[-i] ; // leaves out observation i for out of sample
     yv = y[i] ; // obs i
-    xv = x[-i] ; // obs i
-    ls = fastLm(xt, yt) ; // check the intercept here
+    xv = x[i] ; // obs i
+//    ls = fastLm(xt, yt) ; // check the intercept here
 //    sig = R::summary(ls)$sigma ; // dispersion parameter
 //    cvll_ls[i] = R::dnorm(yv - R::rbind(xv) %*% R::coef(ls),
 //               sd = sig, log = TRUE) ;
   }
 
-  return cvll_ls ;
+  return yv ;
 }
