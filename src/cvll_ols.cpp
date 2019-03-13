95d47854cdc3e76fb243b2d1cfae4eb456c22b31
Shana Scogin <scogins@gmail.com>
2019-03-13 18:04:19 -0400

    Updated PDR for normal

--- src/cvll_ols.cpp
+++ src/cvll_ols.cpp
@@ -1,8 +1,10 @@
 //[[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
+#include <math.h> // for log
 #include <iostream> // for cout debugging
 using namespace Rcpp;
 using namespace std;
+
 
 // [[Rcpp::export]]
 arma::vec cvll_ols(arma::dmat x, arma::mat y, int n_row, int n_col) {
@@ -28,17 +30,17 @@ arma::vec cvll_ols(arma::dmat x, arma::mat y, int n_row, int n_col) {
 //    cout << x << endl ;
     coef = arma::solve(x, y); // fit model y ~ X
     resid = y - x * coef; // residuals
-    coef.shed_row(0); // remove intercept
     sig2 = arma::as_scalar( arma::trans(resid)*resid/(n - n_col) ); // SE of est
     // ?? does this k include the ones????
-    norm = xv * coef;
+    norm = log((1 / sqrt(2 * M_PI * sig2)) *
+      exp(-((yv - xv * coef) * exp(2)) / (2 * sig2)));
 //    cvll_ls[i] = Rcpp::sugar::dnorm(yv - xv * coef, sig2) ;
-//    cvll_ls[i] =  (1 / (2 * sig2)); // * exp(-abs((yv - xv * coef) / sig2)));
+//    cvll_ls[i] =  (1 / (2 * sig2)) * exp(-abs((norm) / sig2)));
     y.insert_rows(i, rowyi); // add y back in
     x.insert_rows(i, rowxi); // add x back in
   }
 
-  return norm ;
+  return norm;
 }
 
 /*** R
@@ -50,8 +52,9 @@ rcvll_ols <- function(x, y){ # cross-validated log likelihoods
     yv <- y[i]
     xv <- x[i, ]
     ls <- lm(yt ~ -1 + xt) # -1 takes out the intercept (1 is identifier)
+    ls2 <- lm(yt ~ xt)
       sig <- summary(ls)$sigma # dispersion parameter
-      cvll_ls[i] <- dnorm(yv - rbind(xv) %*% coef(ls), sd = sig, log = TRUE)
+      um <- dnorm(yv - rbind(xv) %*% coef(ls), sd = sig, log = TRUE)
       norm <- yv - rbind(xv) %*% coef(ls)
       normsm <- rbind(xv) %*% coef(ls)
       normsmb <- xv %*% coef(ls)
@@ -62,7 +65,11 @@ rcvll_ols <- function(x, y){ # cross-validated log likelihoods
               sig = sig,
               norm = norm,
               normsm = normsm,
-              normsmb = normsmb)) # number of missing observations
+              normsmb = normsmb,
+              xv = xv,
+              ls2 = ls2,
+              um = um,
+              coef = coef(ls))) # number of missing observations
 }
 */
 
