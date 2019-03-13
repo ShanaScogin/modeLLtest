2326a4844f417e31bc611ba31232da9ed3072da8
Shana Scogin <scogins@gmail.com>
2019-03-13 00:57:44 -0400

    Added x data back in

--- src/cvll_ols.cpp
+++ src/cvll_ols.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //using namespace arma;
 
 // [[Rcpp::export]]
-arma::dmat cvll_ols(arma::dmat x, NumericVector y, int n_row) { //change y back to numericvector
+arma::dmat cvll_ols(arma::dmat x, NumericVector y, int n_row) {
 
 //  int n_row = y.size() ;
 //  NumericVector cvll_ls(n_row) ;
@@ -20,17 +20,17 @@ arma::dmat cvll_ols(arma::dmat x, NumericVector y, int n_row) { //change y back 
 
   // resource: https://teuder.github.io/rcpp4everyone_en/100_matrix.html
   for (int i = 0; i < n_row; i++) {
-    cout << i ;
 //    yt = y.shed_row(i) ; // leaves out observation i
     arma::rowvec rowi = x.row(i) ;
     x.shed_row(i) ; // leaves out observation i
-    cout << x << endl ;
+//    cout << x << endl ;
 //    yv = y[i] ; // obs i
 //    xv = x.row(i) ; // obs i
 //    ls = fastLm(xt, yt) ; // check the intercept here
 //    sig = R::summary(ls)$sigma ; // dispersion parameter
 //    cvll_ls[i] = R::dnorm(yv - R::rbind(xv) %*% R::coef(ls),
 //               sd = sig, log = TRUE) ;
+    x.insert_rows(i, rowi) ;
   }
 
   return x ;
