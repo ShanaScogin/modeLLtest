e8d0bb7b8925e5d63d289d12907894907c783cef
Shana Scogin <scogins@gmail.com>
2019-03-13 05:21:10 -0400

    Added manual linear fit pre-troubleshoot

--- src/cvll_ols.cpp
+++ src/cvll_ols.cpp
@@ -1,23 +1,21 @@
-#include <RcppArmadillo.h>
-#include <iostream>
-// #include <RcppEigen.h> // for arma::solve or fastLm or something? not sure if nec
 //[[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+//#include <iostream>
+//#include <RcppEigen.h> // for fastLm? not sure if nec
 using namespace Rcpp;
 using namespace std;
 //using namespace arma;
 
 // [[Rcpp::export]]
-List cvll_ols(arma::dmat x, arma::mat y, int n_row) {
+List cvll_ols(arma::dmat x, arma::mat y, int n_row, int n_col) {
 
 //  NumericVector cvll_ls(n_row) ;
-//  arma::dmat yt ;
-//  arma::dmat xt ;
   arma::rowvec rowxi ;
   arma::rowvec rowyi ;
   arma::dmat yv ; // this is actually a double but cpp gets angry bc y is vector
   arma::dmat xv ; // should this be vector? it's vector of ivs
   List ls ;
-//  double sig ;
+  double sig ;
 
   for (int i = 0; i < n_row; i++) {
     yv = y.row(i) ; // defining obs i before change y
@@ -27,9 +25,12 @@ List cvll_ols(arma::dmat x, arma::mat y, int n_row) {
     rowxi = x.row(i) ;
     x.shed_row(i) ; // leaves out observation i but changes x
 //    cout << x << endl ;
-    ls = arma::solve(x, y) ; // check the intercept here
-//    sig = R::summary(ls)$sigma ; // dispersion parameter
-//    cvll_ls[i] = R::dnorm(yv - R::rbind(xv) %*% R::coef(ls),
+//    ls = arma::solve(x, y) ; // check the intercept here
+    ls = 1 / ((arma::inplace_trans(x) * x) * (arma::inplace_trans(x) * y))
+    sig = arma::inplace_trans(y - (x * ls)) * y - (x * ls)) / (n_row - n_col)
+//    sig = arma::mean(abs(residuals(ls))) // dispersion parameter
+//the square root of the estimated variance of the random error
+//    cvll_ls[i] = R::dnorm(yv - R::rbind(xv) %*% ls,
 //               sd = sig, log = TRUE) ;
     y.insert_rows(i, rowyi) ; // adding rest of y back in
     x.insert_rows(i, rowxi) ; // adding rest of x back in
@@ -37,3 +38,4 @@ List cvll_ols(arma::dmat x, arma::mat y, int n_row) {
 
   return ls ;
 }
+
