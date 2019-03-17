edc02efd59721df53f9969a3638c3a06f5fdac3e
Shana Scogin <scogins@gmail.com>
2019-03-16 21:27:40 -0400

    Updated coefs and resid in MR

--- src/cvll_mr.cpp
+++ src/cvll_mr.cpp
@@ -5,18 +5,19 @@
 using namespace Rcpp;
 using namespace std;
 
-// [[Rcpp::export]]
-SEXP cvll_mr(arma::dmat &x, arma::colvec &y, int n_row, int n_col) {
 
-//  int n = n_row - 1;
+// [[Rcpp::export]]
+List cvll_mr(arma::dmat &x, arma::colvec &y, int n_row, int n_col) {
+
+  int n = n_row - 1;
   arma::dmat yv;
   arma::dmat xv;
   arma::rowvec rowyi;
   arma::rowvec rowxi;
-  SEXP mr;
-  arma::dmat coef;
-//  arma::colvec resid;
-//  double b;
+  List mr;
+  List coef;
+  List resid;
+  double b;
   List cvll_mr(n_row);
 
   Rcpp::Function rq = Environment("package:quantreg")["rq.fit"];
@@ -29,18 +30,18 @@ SEXP cvll_mr(arma::dmat &x, arma::colvec &y, int n_row, int n_col) {
     rowxi = x.row(i);
     x.shed_row(i); // leaves out observation i but changes x
     mr = rq(x, y);
-    coef = mr.coefficients;
-//    coef = arma::solve(x, y); // fit model y ~ x
-//    resid = y - x * coef; // residuals
-//    b = arma::as_scalar( arma::trans(resid)*resid/(n - n_col) ); // dispersion param
+    coef = mr("coefficients");
+    resid = mr("residuals"); // residuals
+    b = arma::as_scalar( arma::trans(resid) * resid / (n - n_col) ); // dispersion param
 //    cvll_mr[i] = log( (1 / (2 * b) ) *
 //      exp( -abs( (yv - xv * coef) / b ) ) );
     y.insert_rows(i, rowyi); // add y back in
     x.insert_rows(i, rowxi); // add x back in
   }
 
-  return mr;
+  return resid;
 }
+
 
 
 /*** R
@@ -59,7 +60,7 @@ rcvll_mr <- function(x, y){ # cross-validated log likelihoods
       b <- mean(abs(residuals(mr))) # dispersion parameter
       cvll_mr[i] <- log(dlapl(yv - rbind(xv) %*% coef(mr), b = b))
   }
-  return(mr$coefficients) # number of missing observations
+  return(mr$residuals) # number of missing observations
 }
 dlapl <- function(a, b){
   return(1 / (2 * b) * exp(-abs(a / b)))
--- src/cvll_ols.cpp
+++ src/cvll_ols.cpp
@@ -15,7 +15,6 @@ List cvll_ols(arma::dmat &x, arma::mat &y, int n_row, int n_col) {
   arma::dmat coef;
   arma::colvec resid;
   double sig2;
-  List norm(n_row);
   List cvll_ls(n_row);
 
   for (int i = 0; i < n_row; i++) {
@@ -27,7 +26,7 @@ List cvll_ols(arma::dmat &x, arma::mat &y, int n_row, int n_col) {
     x.shed_row(i); // leaves out observation i but changes x
     coef = arma::solve(x, y); // fit model y ~ x
     resid = y - x * coef; // residuals
-    sig2 = arma::as_scalar( arma::trans(resid)*resid/(n - n_col) ); // SE of est
+    sig2 = arma::as_scalar( arma::trans(resid) * resid /  (n - n_col) ); // SE of est
     cvll_ls[i] = log(arma::normpdf(yv - xv * coef, 0, sig2));
     y.insert_rows(i, rowyi); // add y back in
     x.insert_rows(i, rowxi); // add x back in
