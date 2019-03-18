56fcb158c5dd5b72f8a32f79d69179234d903b81
Shana Scogin <scogins@gmail.com>
2019-03-18 15:20:25 -0400

    Updated citations

--- R/citation.R
+++ R/citation.R
@@ -2,10 +2,8 @@
   packageStartupMessage("ModeLLtest: Compare models with Cross Validated Log Likelihood
                         Shana Scogin, University of Notre Dame
                         Sarah Petersen, University of Notre Dame
-                        Bruce A. Desmarais, Penn State University
                         Jeff Harden, University of Notre Dame
+                        Bruce A. Desmarais, Penn State University
                         Type help('modeLLtest') to get started.
                         Development website: https://github.com/ShanaScogin/modeLLtest")
 }
-
-repo <- git2r::repository(".", discover = TRUE)
