6be548f5e2339fe5e34cd03d1f8ca8807712c256
Shana Scogin <scogins@gmail.com>
2019-03-19 19:20:10 -0400

    Added cvll_rlm to mr file

--- src/cvll_mr.cpp
+++ src/cvll_mr.cpp
@@ -1,9 +1,7 @@
 //[[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
-using namespace Rcpp;
 
-
-arma::colvec vec(List a){
+arma::colvec vec(Rcpp::List a){
   int size = a.size();
   arma::colvec a1(size);
   for (int i = 0; i < size; i++) {
@@ -12,21 +10,29 @@ arma::colvec vec(List a){
   return a1;
 }
 
+Rcpp::List sexpl(arma::dmat &y){
+  int size = y.size();
+  Rcpp::List a1(size);
+  for (int i = 0; i < size; i++) {
+    a1[i] = y[i];
+  }
+  return a1;
+}
 
 // [[Rcpp::export]]
-List cvll_mr(arma::dmat &x, arma::colvec &y, int n_row) {
+Rcpp::List cvll_mr(arma::dmat &x, arma::dmat &y, int n_row) {
 
   arma::dmat yv;
   arma::dmat xv;
   arma::rowvec rowyi;
   arma::rowvec rowxi;
-  List mr;
+  Rcpp::List mr;
   arma::colvec coef;
   arma::colvec resid;
   double b;
-  List cvll_mr(n_row);
+  Rcpp::List cvll_mr(n_row);
 
-  Rcpp::Function rq = Environment("package:quantreg")["rq.fit"];
+  Rcpp::Function rq = Rcpp::Environment("package:quantreg")["rq.fit"];
 
   for (int i = 0; i < n_row; i++) {
     yv = y.row(i); // define obs i before change y
@@ -35,7 +41,7 @@ List cvll_mr(arma::dmat &x, arma::colvec &y, int n_row) {
     xv = x.row(i); // define obs i before change x
     rowxi = x.row(i);
     x.shed_row(i); // leaves out observation i but changes x
-    mr = rq(x, y);
+    mr = rq(sexpl(x), sexpl(y));
     coef = vec(mr("coefficients"));
     resid = vec(mr("residuals")); // residuals
     b = arma::as_scalar( mean(abs(resid)) ); // dispersion param
@@ -47,3 +53,39 @@ List cvll_mr(arma::dmat &x, arma::colvec &y, int n_row) {
 
   return cvll_mr;
 }
+
+// [[Rcpp::export]]
+Rcpp::List cvll_rlm(arma::dmat &x, arma::colvec &y, int n_row, int n_col) {
+
+  int n = n_row - 1;
+  arma::dmat yv;
+  arma::dmat xv;
+  arma::rowvec rowyi;
+  arma::rowvec rowxi;
+  Rcpp::List rlm;
+  arma::colvec coef;
+  arma::colvec resid;
+  double sig2;
+  Rcpp::List cvll_rlm(n_row);
+
+  Rcpp::Function rlm_fit = Rcpp::Environment("package:modeLLtest")["rlm_fit"];
+
+  for (int i = 0; i < n_row; i++) {
+    yv = y.row(i); // define obs i before change y
+    rowyi = y.row(i);
+    y.shed_row(i); // leaves out observation i
+    xv = x.row(i); // define obs i before change x
+    rowxi = x.row(i);
+    x.shed_row(i); // leaves out observation i but changes x
+    rlm = rlm_fit(x, y);
+    coef = vec(rlm("coefficients"));
+    resid = vec(rlm("residuals")); // residuals
+    sig2 = arma::as_scalar( arma::trans(resid) * resid /  (n - n_col) ); // SE of est
+    cvll_rlm[i] = log(arma::normpdf(yv - xv * coef, 0, sig2));
+    y.insert_rows(i, rowyi); // add y back in
+    x.insert_rows(i, rowxi); // add x back in
+  }
+
+  return cvll_rlm;
+}
+
