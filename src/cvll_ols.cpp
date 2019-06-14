aaeeb493d2c6c946cd821c0ad186e2103accb269
Shana Scogin <scogins@gmail.com>
2019-06-14 17:34:09 -0400

    Fixed singular test in ols rcpp

--- src/cvll_ols.cpp
+++ src/cvll_ols.cpp
@@ -2,7 +2,7 @@
 #include <RcppArmadillo.h>
 
 // [[Rcpp::export]]
-arma::dmat cvll_ols(arma::dmat &x, arma::vec &y, int n_row, int n_col) {
+Rcpp::List cvll_ols(arma::dmat &x, arma::vec &y, int n_row, int n_col) {
 
   int n = n_row - 1;
   arma::dmat yv;
@@ -13,8 +13,6 @@ arma::dmat cvll_ols(arma::dmat &x, arma::vec &y, int n_row, int n_col) {
   arma::colvec resid;
   double sig;
   Rcpp::List cvll_ls(n_row);
-  arma::dmat test;
-  arma::dmat Q, R;
 
   for (int i = 0; i < n_row; i++) {
     rowyi = y.row(i); // define obs i before change y
@@ -22,26 +20,32 @@ arma::dmat cvll_ols(arma::dmat &x, arma::vec &y, int n_row, int n_col) {
     rowxi = x.row(i); // define obs i before change x
     x.shed_row(i); // leaves out observation i but changes x
 
-    // checking for singularity
-    // if(arma::rank(arma::qr(x)) < n_col)
-    // {}
-    // else
-    // {}
+    // wrap in for statement for singularity
+    if(arma::rank(x) < n_col) {
 
-    test = arma::qr(Q, R, x);
+      // cleaning up
+      y.insert_rows(i, rowyi); // add y back in
+      x.insert_rows(i, rowxi); // add x back in
 
-    // model
-    coef = arma::solve(x, y); // fit model y ~ x
-    resid = y - x * coef; // residuals
-    sig = arma::as_scalar(sqrt( arma::trans(resid) * resid /  (n - n_col) )); // sqrt(SE of est)
+      continue;
 
-    // output
-    cvll_ls[i] = log(arma::normpdf(rowyi - rowxi * coef, 0, sig));
+    } else {
 
-    // cleaning up
-    y.insert_rows(i, rowyi); // add y back in
-    x.insert_rows(i, rowxi); // add x back in
+      // model
+      coef = arma::solve(x, y); // fit model y ~ x
+      resid = y - x * coef; // residuals
+      sig = arma::as_scalar(sqrt( arma::trans(resid) * resid /  (n - n_col) )); // sqrt(SE of est)
+
+      // output
+      cvll_ls[i] = log(arma::normpdf(rowyi - rowxi * coef, 0, sig));
+
+      // cleaning up
+      y.insert_rows(i, rowyi); // add y back in
+      x.insert_rows(i, rowxi); // add x back in
+
+    }
+
   }
 
-  return test;
+  return cvll_ls;
 }
