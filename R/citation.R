6a5ea65c0073bb0d35ea2c33971269f11934efee
Shana Scogin <scogins@gmail.com>
2019-09-03 09:43:30 -0400

    updated citation to include JOSS

--- R/citation.R
+++ R/citation.R
@@ -1,9 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("modeLLtest: Compare models with Cross-Validated Log-Likelihood
-                        Shana Scogin, University of Notre Dame
-                        Sarah Petersen, University of Notre Dame
-                        Jeff Harden, University of Notre Dame
-                        Bruce A. Desmarais, Penn State University
-                        Type help('modeLLtest') to get started.
-                        Development website: https://github.com/ShanaScogin/modeLLtest")
+  packageStartupMessage("Please cite: Scogin et al., (2019). modeLLtest: An R Package for Unbiased
+                        Model Comparison using Cross Validation. Journal of Open Source Software,
+                        4(41), 1542, https://doi.org/10.21105/joss.01542")
 }
